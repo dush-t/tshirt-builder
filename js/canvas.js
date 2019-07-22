b47b19e9e0d40d46eef58cde9ee4f7a6271fd363
Dushyant Yadav <dushyant9309@gmail.com>
2019-07-22 22:33:20 +0530

    Playing around

---
+++ js/canvas.js
@@ -0,0 +1,23 @@
+// let width = document.querySelector('.fabric-container')[0].offsetWidth;
+// let height = document.querySelector('.fabric-container')[0].offsetHeight;
+
+let canvas = new fabric.Canvas('mainCanvas');
+
+
+
+// canvas.setBackgroundImage('https://png2.kisspng.com/sh/fc4ff5ab32a474f66cd6bd7062c1f70d/L0KzQYm3UsA0N5Z6j5H0aYP2gLBuTgBzcZ95fdY2dD32eLr5lL10dJZqjtc2Y3zyhLnwjvcuap1mht02d3jshLa0lL10cJp3jJ99ZX3zfLL7hb1xdpgyTdMCNUTlRYrrVsQ0a2YzSagBNUO8QIG4VcE4PmQ7TKY6OEe6RnB3jvc=/kisspng-printed-t-shirt-sleeve-clothing-blank-white-t-shirt-template-png-5a754b59d643c5.1665390015176364418776.png',
+//     canvas.renderAll.bind(canvas), {
+//         scaleX: 1,
+//         scaleY: 1,
+//     })
+
+var rect = new fabric.Rect({
+    left: 100,
+    top: 100,
+    fill: 'red',
+    width: 20,
+    height: 20,
+    angle: 45
+  });
+
+canvas.add(rect); // add object
