aff46e20a6753fecda862bbc7ececd8d75137231
Shreyans Jain <shreyansj551@gmail.com>
2019-08-08 09:17:59 +0530

    add undo-redo functionality

--- js/toolbar.js
+++ js/toolbar.js
@@ -11,9 +11,15 @@ const mountContent = i => {
       break;
 
     case 2:
-      let title2 = addHeading(i-1);
+      let title2 = addHeading(i - 1);
       parentDiv.appendChild(title2);
       parentDiv.appendChild(page2);
+      break;
+
+    case 3:
+      let title3 = addHeading(i - 1);
+      parentDiv.appendChild(title3);
+      parentDiv.appendChild(page3);
       break;
 
     default:
@@ -23,8 +29,8 @@ const mountContent = i => {
 
 const headers = [
   "Add Text",
-  "Artwork Categories",
-  "Upload",
+  "Add Image",
+  "Add SVG",
   "Choose your product colors",
   "Name and numbers",
   "Notes"
@@ -56,7 +62,17 @@ page2.innerHTML = `
     <input type="text" placeholder="Enter url here" class="page2-input">
     <button onclick="addImage(document.getElementsByClassName('page2-input')[0].value,1)">Add to Design</button>
 </div>
-`
+`;
+
+// ------------ add svg ---------
+const page3 = document.createElement("div");
+page3.classList.add("page1");
+page3.innerHTML = `
+<div class="page1">
+    <input type="text" placeholder="Enter url here" class="page2-input">
+    <button onclick="addSVG(document.getElementsByClassName('page2-input')[0].value)">Add to Design</button>
+</div>
+`;
 
 // --------------------------
 
@@ -69,3 +85,11 @@ for (let i = 1; i < 7; i++) {
   tools.push(tool);
   tool.addEventListener("click", () => mountContent(i));
 }
+
+// ------------------- Undo/Redo functionality
+
+const undoButton = document.getElementsByClassName("undo-button")[0];
+const redoButton = document.getElementsByClassName("redo-button")[0];
+
+undoButton.setAttribute("onclick", "undo()");
+redoButton.setAttribute("onclick", "redo()");
